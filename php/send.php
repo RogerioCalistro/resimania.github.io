05d04ec433079a1445495d421a5377d62a6ae0ce
RogerioCalistro <34359075+RogerioCalistro@users.noreply.github.com>
2024-09-12 16:42:44 -0300

    Realizado alterações nos código css - index - php

--- php/send.php
+++ php/send.php
@@ -1,29 +1,47 @@
 <?php
+    
+    ini_set( 'display_errors', 1 );
+    error_reporting( E_ALL );
+    
+    $name = $_POST["nome"];
+    $email = $_POST["email"];
+    $assunto = $_POST["assunto"];
+    $telefone = $_POST["telefone"];
+    $mensagem = $_POST["mensagem"];
 
-use php\src\PHPMailer\Exception;
-use php\src\PHPMailer\PHPMailer;
-use php\src\PHPMailer\SMTP;
+    require '../php/PHPMailer/src/Exception.php';
+    require '../php/PHPMailer/src/PHPMailer.php';
+    require '../php/PHPMailer/src/SMTP.php';
 
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
 
-require 'php\src\PHPMailer\Exception.php';
-require 'php\src\PHPMailer\PHPMailer.php';
-require 'php\src\PHPMailer\SMTP.php';
+    $mail = new PHPMailer (true);
+    $mail->SMTPDebug  = 0;
 
-require 'config.php';
-
-function sendMail($nome, $email, $assunto, $telefone, $mensagem) {
-    $mail =  new PHPMailer(true);
+    try {
+    
     $mail->isSMTP();
     $mail->SMTPAuth = true;
 
-    $mail->Host = MAILHOST;
-    $mail->Username = USERNAME;
-    $mail->Password = PASSWORD;
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Username = 'jvm.api@gmail.com';
+    $mail->Password = 'wtzmdcylpttodvee';
 
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = 587;
-    $mail->setForm(SEND_FROM, SEND_FROM_NAME);
+    $mail->addAddress('jvm.api@gmail.com');
 
-    $mail->addAddress($mail);
-    $mail->addReplyTo(REPLAY_TO, REPLAY_TO_NAME);
-}
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->Port = 465;
+    $mail->setFrom('desenvolvedor.calistro@gmail.com', 'Admin');
+  
+    $mail->Body.= "De: ".($_POST['nome'])."\n";
+    $mail->Body.= "E-mail: ".($_POST['email'])."\n";
+    $mail->Body.= "Assunto: ".($_POST['assunto'])."\n";
+    $mail->Body.= "Mensagem: ".($_POST['mensagem'])."\n";
+
+    $mail->send();
+        echo "Ola $name, E-mail enviado com sucesso !";
+    } catch (Exception $e) {
+        echo "Olá $name, não foi possível enviar a mensagem. Erro no envio: {$mail->ErrorInfo}";
+    }     
+    $mail->smtpClose();
